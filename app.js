9e43320f1ccc20e98c29af992aaeee3d282f61ab
yceline-yu <cellystuff5@gmail.com>
2021-05-12 12:09:00 -0700

    routes

--- app.js
+++ app.js
@@ -8,15 +8,45 @@ const { NotFoundError } = require("./expressError");
 
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
+const { findMean, findMedian, findMode } = require("./stats")
+
+const { convertStrNums } = require("./utils")
+
+// process JSON body => req.body
+app.use(express.json());
+
+// process traditional form data => req.body
+app.use(express.urlencoded({ extended: true }));
+
 
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
+app.get("/mean", function (req, res){
+    let nums = convertStrNums(req.query)
+    let mean = findMean(nums)
+
+    return res.json({response: {operation: "mean", value: mean}})
+
+})
 
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
+app.get("/median", function (req, res){
+  let nums = convertStrNums(req.query)
+  let median = findMedian(nums)
+
+  return res.json({response: {operation: "median", value: median}})
+
+})
 
 
 /** Finds mode of nums in qs: returns {operation: "mean", result } */
+app.get("/mode", function (req, res){
+  let nums = convertStrNums(req.query)
+  let mode = findMode(nums)
 
+  return res.json({response: {operation: "mode", value: mode}})
+
+})
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res, next) {
@@ -30,7 +60,6 @@ app.use(function (err, req, res, next) {
   if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
   return res.status(status).json({ error: { message, status } });
 });
-
 
 
 module.exports = app;
